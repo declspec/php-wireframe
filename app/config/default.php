63cbc05e949cc661c9b171070a3687aa70c96c0e
Jason Larke <jasonlarke@live.com.au>
2016-02-19 09:34:22 +0800

    Added a default config with some basic structure

---
+++ app/config/default.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    "appName" => "New Application",
+    "baseUrl" => null,
+    
+    "db" => array(
+        "connectionString" => "<connection-string>",
+        "username" => "<username>",
+        "password" => "<password>"
+    )  
+);
