15dca33b47108d67db921c1978dd0789e838387b
Jason Larke <jasonlarke@live.com.au>
2016-04-05 16:10:40 +0800

    Removed application-specific JS code

--- assets/js/common/config.js
+++ assets/js/common/config.js
@@ -3,25 +3,15 @@
     
     var common = angular.module("common", []);
     
-    common.config(["$provide", "$httpProvider", function($provide, $httpProvider) {
-        $provide.constant("$user", window.user || null);
-        
+    common.config(["$httpProvider", function($httpProvider) {
         // Allow through any response that is in the expected API format.
-        // also redirect the user when an unauthorized error comes through
-        $httpProvider.interceptors.push(["$q", "$window", function($q, $window) {
+        $httpProvider.interceptors.push(["$q", function($q) {
             return {
                 responseError: function(rejection) {
                     if ("object" !== typeof(rejection.data) || !rejection.data.hasOwnProperty("success"))
                         return $q.reject(rejection);
                     
-                    if (rejection.data.status !== 401)
-                        return rejection;
-                    
-                    // If it's an 'Unauthorized' error then it may be due to the user's access token expiring.
-                    // redirect them to the login page.
-                    var target = $window.location.pathname + $window.location.search;
-                    $window.location.href = "/auth/login?return=" + target.split("/").map(encodeURIComponent).join("/");
-                    return $q.reject(rejection);
+                    return rejection;
                 }
             };
         }]);
